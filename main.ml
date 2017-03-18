8af9b8e92dc3274338a5b101ffef32b2c9dc1c4a
George Tucker <georgetucker1@hotmail.co.uk>
2017-03-18 17:34:06 +0000

    Removed unused code in main

--- main.ml
+++ main.ml
@@ -7,20 +7,16 @@ open Printf
 let channel =
     try (open_in Sys.argv.(1))
     with Invalid_argument x -> failwith "USAGE: ./langlangi PATH";;
+
 let parseProgram =
     try let lexbuf = Lexing.from_channel channel in
         parse_main language lexbuf
     with Parsing.Parse_error -> failwith "Parse Failure";;
 
-(*
-let arg = ref stdin in
-    let setProg prog = arg := open_in prog in
-        let usage = "./langlang PROGRAM" in
-            parse [] setProg; *)
-            let parsedProg = parseProgram in
-                let () = print_string "Parsing Completed\n" in
-                    let _ = typeOfProgram parsedProg in
-                        let () = print_string "Type Checking Completed\n" in
-                            let result = evalProg parsedProg in
-                                let () = print_string "Result of program execution: " ; print_val result in
-                                    flush stdout;;
+let parsedProg = parseProgram in
+    let () = print_string "Parsing Completed\n" in
+        let _ = typeOfProgram parsedProg in
+            let () = print_string "Type Checking Completed\n" in
+                let result = evalProg parsedProg in
+                    let () = print_string "Result: " ; print_val result in
+                        flush stdout;;
